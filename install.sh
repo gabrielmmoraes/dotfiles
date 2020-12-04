b4934a909f3ed0a1785521deee0daa1e044687ae
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2020-12-04 14:39:30 -0300

    Adicionando ripgrep ao script de instalação
    Usado para busca no Emacs

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ yay -S git firefox google-chrome discord pulseaudio pavucontrol \
     flashfocus python3 visual-studio-code-bin flameshot spotify \
     gitkraken steam ttf-font-awesome-4 powerline termite polybar \
     ttf-iosevka zsh rofi picom npm go feh yarn cmake playerctl \
-    zsh-syntax-highlighting betterlockscreen emacs the_silver_searcher
+    zsh-syntax-highlighting betterlockscreen emacs the_silver_searcher \
+    ripgrep
 
 echo "Copiando config do i3wm"
 rm  ~/.i3/config
